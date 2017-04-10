37db1e1721a75887ba6b33bdde527799dbc4be9c
Konrad Zapałowicz <konrad.zapalowicz@canonical.com>
2017-04-10 21:59:47 +0200

    examples: list of repositories is now dynamic

--- examples/flightschedule/main.go
+++ examples/flightschedule/main.go
@@ -11,26 +11,6 @@ import (
 	"text/tabwriter"
 )
 
-var snaps = []string{
-	"alsa-utils",
-	"bluez",
-	"build-scripts",
-	"captive-redirect",
-	"engineering-tests",
-	"git-repo",
-	"modem-manager",
-	"network-manager",
-	"pulseaudio",
-	"se-test-tools",
-	"tests-extras",
-	"tpm",
-	"tpm2",
-	"udisks2",
-	"upower",
-	"wifi-ap",
-	"wpa-supplicant",
-}
-
 func main() {
 
 	secrets, err := launchpad.NewLaunchpadSecrets("./launchpad.secrets.json")
@@ -44,12 +24,18 @@ func main() {
 		return
 	}
 
-	log.Println("Logged in to Launchpad")
+	team, err := lp.People("snappy-hwe-team")
+	if err != nil {
+		log.Fatal("lp.People: ", err)
+		return
+	}
+
+	snaps, err := lp.GitRepositories().GetRepositories(team.SelfLink)
 
 	w := tabwriter.NewWriter(os.Stdout, 0, 0, 1, ' ', 0)
 
 	for _, s := range snaps {
-		gitrepository, err := lp.GitRepositories().GetByPath("~snappy-hwe-team/snappy-hwe-snaps/+git/" + s)
+		gitrepository, err := lp.GitRepositories().GetByPath(s.DisplayName[3:])
 		if err != nil {
 			log.Fatal("Failed to get git repository")
 		}
@@ -59,7 +45,7 @@ func main() {
 		}
 		for _, mp := range landingcandidates {
 			fmt.Fprintf(w, " %s\t| %s\t| %s\t| %d\t| %s\n",
-				s,
+				s.Name,
 				mp.RegistrantLink[33:], mp.WebLink,
 				uint(time.Now().Sub(mp.DateCreated).Hours()/24+0.5),
 				mp.QueueStatus)

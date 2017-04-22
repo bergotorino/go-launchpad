08fb626f6fa143ee0cb7584f7145c9d81efe2bf5
Konrad Zapałowicz <konrad.zapalowicz@canonical.com>
2017-04-22 12:27:17 +0200

    lp: refactor secrets
    Introduce the SecretsReaderWriter as an interface to separate the
    actual reading/writing functionality from the data. Now it is easier to
    test.

--- launchpad/launchpadsecrets.go
+++ launchpad/launchpadsecrets.go
@@ -3,12 +3,65 @@ package launchpad
 import (
 	"encoding/json"
 	"github.com/bergotorino/go-oauth/oauth"
+	"io"
 	"io/ioutil"
 )
 
 type LaunchpadSecrets struct {
 	file              string
 	accessCredentials *oauth.Credentials
+}
+
+func (l *LaunchpadSecrets) Write(w io.Writer) error {
+	data, err := json.Marshal(l.accessCredentials)
+	if err != nil {
+		return err
+	}
+
+	_, err = w.Write(data)
+	return err
+}
+
+func (l *LaunchpadSecrets) Read(r io.Reader) error {
+	var data []byte
+	_, err := r.Read(data)
+	if err != nil {
+		return err
+	}
+
+	var ac oauth.Credentials
+	err = json.Unmarshal(data, &ac)
+	if err != nil {
+		return err
+	}
+	l.accessCredentials = &ac
+
+	return nil
+}
+
+type SecretsReaderWriter interface {
+	Write(p []byte) (n int, err error)
+	Read(p []byte) (n int, err error)
+}
+
+type SecretsFileBackend struct {
+	File string
+}
+
+func (s *SecretsFileBackend) Write(data []byte) (n int, err error) {
+	err = ioutil.WriteFile(s.File, data, 0664)
+	if err != nil {
+		return 0, err
+	}
+	return len(data), nil
+}
+
+func (s *SecretsFileBackend) Read(data []byte) (n int, err error) {
+	data, err = ioutil.ReadFile(s.File)
+	if err != nil {
+		return 0, err
+	}
+	return len(data), nil
 }
 
 func NewLaunchpadSecrets(secretsFile string) (*LaunchpadSecrets, error) {
